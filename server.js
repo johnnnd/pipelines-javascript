04835c959c6ff0be674908c254e1ec759a544732
john <john.daoud@cubesys.com.au>
2019-10-28 15:14:49 +1100

    initial

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Node.js Sample!\n');
+  res.send('Hello Node.js Sample! Made by John Daoud\n');
 });
 
 var port = process.env.PORT||PORT;
